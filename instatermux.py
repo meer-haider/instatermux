4afa62182701d5d0d2f3a31a2a00d5d4b5abf84a
meer-haider <161801234+meer-haider@users.noreply.github.com>
2024-03-01 12:26:44 +0530

    instatermux.py

--- instatermux.py
+++ instatermux.py
@@ -1,10 +1,16 @@
 from instaloader import Instaloader
+import sys
+
+# Check if the username is provided as a command-line argument
+if len(sys.argv) != 2:
+    print("Please provide the Instagram username as a command-line argument.")
+    sys.exit(1)
+
+# Get the username from the command-line argument
+profile = sys.argv[1]
 
 # Create an Instaloader instance
 loader = Instaloader()
-
-# Replace 'your_username' with the Instagram username you want to download
-profile = 'your_username'
 
 # Download profile pictures
 loader.download_profile(profile, profile_pic_only=True)
