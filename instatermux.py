65a73126ec4b800521ac1d6a78af0cfb30957ddc
meer-haider <161801234+meer-haider@users.noreply.github.com>
2024-03-01 12:35:20 +0530

    instatermux.py

--- instatermux.py
+++ instatermux.py
@@ -1,5 +1,6 @@
 from instaloader import Instaloader
 import sys
+import datetime
 
 # Check if the username is provided as a command-line argument
 if len(sys.argv) != 2:
@@ -12,5 +13,8 @@ profile = sys.argv[1]
 # Create an Instaloader instance
 loader = Instaloader()
 
-# Download profile pictures
-loader.download_profile(profile, profile_pic_only=True)
+# Get the current timestamp
+timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+
+# Download profile pictures with a unique filename
+loader.download_profile(profile, profile_pic_only=True, filename_pattern=f"{profile}_{timestamp}_UTC_profile_pic.jpg")
